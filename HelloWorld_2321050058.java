eea41f459bb249f58e16f5f3e9d7701692f882e8
caottquyen63 <caottquyen63@gmail.com>
2025-08-15 10:26:49 +0700

    Xóa

--- HelloWorld_2321050058.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld_2321050058 {
-
-    public static void main(String[] args) {
-        System.out.println("Xinchao-CaoThiThuQuyen_2321050058");
-        System.out.println("hello");
-        System.out.println("hellooooooooo");
-    }
-}
